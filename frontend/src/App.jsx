523728a7cdbeb7078184629c191b24fdb429c5ef
Elizabeth Teresa <elizabethteresa1717@gmail.com>
2025-03-23 19:46:18 -0400

    deleted stripe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,12 @@ import "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import BulkOrderForm from "./pages/BulkOrderForm";
-import StripePayment from "./components/StripePayment";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<BulkOrderForm />} />
-        <Route path="/payment" element={<StripePayment />} />
       </Routes>
     </Router>
   );
