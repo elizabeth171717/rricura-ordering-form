38d296daea547f8064202dd6dc3871397c022f1a
Elizabeth Teresa <elizabethteresa1717@gmail.com>
2025-08-22 14:47:11 -0400

    debugging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
