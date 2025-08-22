b976c352c3ee2b82e1d0a8e2499cd4d8f3729d90
Elizabeth Teresa <elizabethteresa1717@gmail.com>
2025-08-22 14:04:25 -0400

    still styling home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,7 +10,15 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import ORDERRAJAS from "../assets/ORDERRAJAS.png";
 import myVideo from "../assets/ordervideo.mp4";
 import ORDERCHICKEN from "../assets/ORDERCHICKEN.png";
-import Corporate from "../assets/corporate.jpg";
+import TamalesTray from "../assets/tamalestray.png";
+import ImgGallery1 from "../assets/chickengreencorn.jpg";
+import ImgGallery2 from "../assets/bananaleafchicken.jpg";
+import ImgGallery3 from "../assets/pintotamale.jpg";
+import ImgGallery4 from "../assets/holiday.jpg";
+import ImgGallery5 from "../assets/family.jpg";
+import ImgGallery6 from "../assets/sweettamale.png";
+import ImgGallery7 from "../assets/rajas.jpg";
+import ImgGallery8 from "../assets/chickentamale.jpg";
 import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import Footer from "../components/Footer";
 
@@ -26,6 +34,9 @@ const HomePage = () => {
       </Helmet>
       <Navigation />
       <div className="hero-section">
+        <div className="hero-title">
+          <h2>WE CATER</h2>
+        </div>
         <div className="icons-container">
           <a
             href="https://www.facebook.com/profile.php?id=61566890440038"
@@ -82,14 +93,24 @@ const HomePage = () => {
             START YOU ORDER
           </Link>
         </div>
+        <div className="hero-img">
+          <img src={TamalesTray} />
+        </div>
       </div>
       <section className="tamales-intro">
-        <div className="full image">
-          <img src={Corporate} />
-        </div>
         <div className="split video">
           <div className="container text-container">
             <h1 className="big-text">HOW TO ORDER</h1>
+            <div className="list">
+              <p>✅Choose Tamale quantity.</p>
+              <p>✅Choose Tamale filling.</p>
+              <p>✅Choose Tamale Wrapper.</p>
+              <p>✅Choose Tamale Sauce.</p>
+              <p>✅Enter delivery details. </p>
+            </div>
+            <div className="home-btn">
+              <Link to="/OnlineOrdering">ORDER NOW</Link>
+            </div>
           </div>
           <div className="container  graphic-container">
             <video width="100%" height="auto" controls>
@@ -98,9 +119,23 @@ const HomePage = () => {
             </video>
           </div>
         </div>
+        <div className="gallery">
+          <img src={ImgGallery1} />
+          <img src={ImgGallery2} />
+          <img src={ImgGallery3} />
+          <img src={ImgGallery4} />
+          <img src={ImgGallery5} />
+          <img src={ImgGallery6} />
+          <img src={ImgGallery7} />
+          <img src={ImgGallery8} />
+        </div>
+
         <div className="full">
           <h1 className="full-text">
-            Enjoy our delicious Tamales with Family, Coworkers and Friends
+            Enjoy our delicious Tamales with{" "}
+            <span className="highlight">Family</span> ,{" "}
+            <span className="highlight">Coworkers</span> and{" "}
+            <span className="highlight">Friends</span>.
           </h1>
         </div>
         <div className="split">
