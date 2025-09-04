b4bc1dd281b252d1a76d36f649924905a17220c4
Elizabeth Teresa <elizabethteresa1717@gmail.com>
2025-09-03 22:57:25 -0400

    homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -35,70 +35,74 @@ const HomePage = () => {
         />
       </Helmet>
       <Navigation />
-      <div className="hero-section">
-        <div className="icons-container">
-          <a
-            href="https://www.facebook.com/profile.php?id=61566890440038"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faFacebook}
-              style={{ fontSize: "1.50rem", color: "white" }}
-            />
-          </a>
-          <a
-            href="https://www.instagram.com/r_ricura/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faInstagram}
-              style={{ fontSize: "1.50rem", color: "white" }}
-            />
-          </a>
-          <a
-            href="https://x.com/Rricuratamales"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faTwitter}
-              style={{ fontSize: "1.50rem", color: "white" }}
-            />
-          </a>
-          <a
-            href="https://www.tiktok.com/@rricuratamales?_t=ZP-8y5G5NDSIQw&_r=1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon
-              icon={faTiktok}
-              style={{ fontSize: "1.50rem", color: "white" }}
-            />
-          </a>
-        </div>
-
-        <div className="hero-title">
-          <h2>Mexican Tamale Catering in Brookhaven & Atlanta</h2>
-        </div>
-        <div className="hero-btn">
-          <FontAwesomeIcon
-            icon={faHandPointDown}
-            className="bouncing-pointer"
-            style={{ color: "#9D0759" }}
-          />
-          <Link to="/OnlineOrdering" className="home-btn">
-            START YOU ORDER
-          </Link>
-        </div>
-        <div className="hero-img">
-          <img src={TamalesTray} />
-        </div>
-      </div>
-      <section className="tamales-intro">
-        <div className="full">
-          <div className="full-text">
+      <div className="sections-container">
+        <section className="contaniner hero-section">
+          <div className="text-container">
+            <div className="icons-container">
+              <a
+                href="https://www.facebook.com/profile.php?id=61566890440038"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faFacebook}
+                  style={{ fontSize: "1.75rem", color: "white" }}
+                />
+              </a>
+              <a
+                href="https://www.instagram.com/r_ricura/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faInstagram}
+                  style={{ fontSize: "1.75rem", color: "white" }}
+                />
+              </a>
+              <a
+                href="https://x.com/Rricuratamales"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faTwitter}
+                  style={{ fontSize: "1.75rem", color: "white" }}
+                />
+              </a>
+              <a
+                href="https://www.tiktok.com/@rricuratamales?_t=ZP-8y5G5NDSIQw&_r=1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon
+                  icon={faTiktok}
+                  style={{ fontSize: "1.75rem", color: "white" }}
+                />
+              </a>
+            </div>
+            <div className="hero-title">
+              <h2>Mexican Tamale Catering in Brookhaven & Atlanta</h2>
+            </div>
+            <div className="hero-btn">
+              <FontAwesomeIcon
+                icon={faHandPointDown}
+                className="bouncing-pointer"
+                style={{ color: "#9D0759" }}
+              />
+              <Link to="/OnlineOrdering" className="home-btn">
+                START YOU ORDER
+              </Link>
+            </div>
+          </div>
+          <div className="graphic-container">
+            {" "}
+            <div className="hero-img">
+              <img src={TamalesTray} />
+            </div>
+          </div>
+        </section>
+        <section className="contaniner about-section">
+          <div className="text-container">
             <h2>Authentic Mexican Catering for Every Occasion</h2>
             <p>
               At Rricura Tamales, we specialize in catering authentic Mexican
@@ -108,10 +112,15 @@ const HomePage = () => {
               we’ll make your event unforgettable.
             </p>
           </div>
-        </div>
-        <div className="split colored-bg">
-          <div className="container text-container">
-            <h1 className="big-text">How to Order Catering</h1>
+          <div className="graphic-container">
+            <Link to="/OnlineOrdering">
+              <img src={ORDERCHICKEN} />
+            </Link>
+          </div>
+        </section>
+        <section className="contaniner video-section">
+          <div className="text-container">
+            <h2>How to Order Catering</h2>
             <p>
               We require a 2-day advance notice for all catering orders.
               Ordering is simple:
@@ -123,29 +132,25 @@ const HomePage = () => {
               <p>✅Choose Tamale Sauce.</p>
               <p>✅Enter delivery details. </p>
             </div>
-            <div className="home-btn">
-              <Link to="/OnlineOrdering">ORDER NOW</Link>
-            </div>
           </div>
-          <div className="container  graphic-container">
+          <div className="graphic-container">
             <video width="100%" height="auto" controls>
               <source src={myVideo} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
           </div>
-        </div>
-        <div className="full">
-          <div className="full-graphic">
-            <div className="subtitle">
-              <h2>Our Catering Menu</h2>
-              <p>
-                We offer traditional tamales: Chicken, Pork, Rajas, Black Beans,
-                Chipillin, Sweet and Fruits. As well as specialty Tamales: Vegan
-                . Make it a combo, add salsa verde and aguas frescas for the
-                full expirience.
-              </p>
-            </div>
-
+        </section>
+        <section className="container">
+          <div className="text-container">
+            <h2>Our Catering Menu</h2>
+            <p>
+              We offer traditional tamales: Chicken, Pork, Rajas, Black Beans,
+              Chipillin, Sweet and Fruits. As well as specialty Tamales: Vegan .
+              Make it a combo, add salsa verde and aguas frescas for the full
+              expirience.
+            </p>
+          </div>
+          <div className="graphic-container">
             <div className="gallery">
               <img src={ImgGallery1} />
               <img src={ImgGallery2} />
@@ -157,38 +162,33 @@ const HomePage = () => {
               <img src={ImgGallery8} />
             </div>
           </div>
-        </div>
-
-        <div className="full ">
+        </section>
+        <section className="container">
           <h1 className="full-text">
             Enjoy our delicious Tamales with{" "}
             <span className="highlight">Family</span> ,{" "}
             <span className="highlight">Coworkers</span> and{" "}
             <span className="highlight">Friends</span>.
           </h1>
-        </div>
-        <div className="split">
-          <div className="container  graphic-container">
-            <Link to="/OnlineOrdering">
-              <img src={ORDERCHICKEN} />
-            </Link>
+        </section>
+
+        <section className="container chooseUs-container">
+          <div className="text-container">
+            {" "}
+            <h2>Why Choose Rricura Tamales for Your Catering?</h2>
+            <div className="list">
+              <p>✅ Authentic homemade Mexican recipes</p>
+              <p>✅ Perfect for groups of 12 to 1000+</p>
+              <p>✅ Fresh, hot, and delivered on time</p>
+            </div>
           </div>
-          <div className="container  graphic-container">
+          <div className="graphic-container">
             <Link to="/OnlineOrdering">
               <img src={ORDERRAJAS} />
             </Link>
           </div>
-        </div>
-        <div className="full colored-bg ">
-          <div className="full-text">
-            <h2>Why Choose Rricura Tamales for Your Catering?</h2>
-            <p>✅ Authentic homemade Mexican recipes</p>
-            <p>✅ Perfect for groups of 12 to 1000+</p>
-            <p>✅ Fresh, hot, and delivered on time</p>
-          </div>
-        </div>
-      </section>
-
+        </section>
+      </div>
       <Footer />
     </div>
   );
